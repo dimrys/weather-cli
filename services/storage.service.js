a8386ff2589a742f9882d672649c9a85384fe1c9
dimrys <akkauntdlyraboti@gmail.com>
2021-12-19 12:06:46 +0300

    add api.service

--- services/storage.service.js
+++ services/storage.service.js
@@ -4,6 +4,10 @@ import {promises} from 'fs'
 
 
 const filePath = join(homedir(), 'weather-data.json')
+const TOKEN_DICTIONARY = {
+    token: 'token',
+    city: 'city'
+}
 
 const saveKeyValue = async (key, value) => {
     let data = {}
@@ -33,4 +37,4 @@ const isExist = async (path) => {
     }
 
 }
-export {saveKeyValue, getKeyValue}
+export {saveKeyValue, getKeyValue, TOKEN_DICTIONARY}
--- weather.js
+++ weather.js
@@ -1,11 +1,16 @@
 #!/usr/bin/env node
 import { getArgs} from "./helpers/args.js"
 import {printHelp, printSuccess, printError} from "./services/log.service.js";
-import {saveKeyValue} from "./services/storage.service.js";
+import {saveKeyValue, TOKEN_DICTIONARY} from "./services/storage.service.js";
+import {getWeather} from "./services/api.service.js";
 
 const saveToken = async (token) => {
+    if (!token.length) {
+        printError('Не передан токен')
+        return
+    }
     try {
-        await saveKeyValue('token', token)
+        await saveKeyValue(TOKEN_DICTIONARY.token, token)
         printSuccess('Токен сохранен')
     } catch (e) {
         printError(e.message)
@@ -24,7 +29,7 @@ const initCli = () => {
     if(args.t) {
         return saveToken(args.t)
     }
-
+    getWeather('moscow')
 }
 
 initCli()
